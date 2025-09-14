fdc47970c8494bb91aaa4ba33dde9435b8f50be3
Jayden Liaw <jianwei077@gmail.com>
2025-09-14 23:19:22 +0800

    01-insert-schema generates fixes

---
+++ it5008_project/notebook/menu_inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO menu (Item, Price, Cuisine) VALUES ('Rendang', '4.0', 'Indonesian');
+INSERT INTO menu (Item, Price, Cuisine) VALUES ('Ayam Balado', '4.0', 'Indonesian');
+INSERT INTO menu (Item, Price, Cuisine) VALUES ('Gudeg', '3.0', 'Indonesian');
+INSERT INTO menu (Item, Price, Cuisine) VALUES ('Rinderrouladen', '3.5', 'German');
+INSERT INTO menu (Item, Price, Cuisine) VALUES ('Sauerbraten', '4.0', 'German');
+INSERT INTO menu (Item, Price, Cuisine) VALUES ('Bun Cha', '3.0', 'Vietnamese');
+INSERT INTO menu (Item, Price, Cuisine) VALUES ('Thunder Tea Rice', '2.5', 'Chinese');
+INSERT INTO menu (Item, Price, Cuisine) VALUES ('Palak Paneer', '4.0', 'Indian');
